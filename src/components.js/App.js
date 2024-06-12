8a2a3a5588808355cde860839e3394b8047d3a82
Rince Mathew <rincemathewmanattu@gmail.com>
2024-06-12 22:17:49 +0530

    new todo start

---
+++ src/components.js/App.js
@@ -0,0 +1,15 @@
+
+import Header from './Header';
+import AddContact from './AddContact';
+import ContactList from './ContactList';
+
+function App() {
+  return (
+    <div className="App">
+      <div>Hello world </div>
+      
+    </div>
+  );
+}
+
+export default App;
