3404383d965faf2c762eb1dcd7ddf9b3b706d655
Rince Mathew <rincemathewmanattu@gmail.com>
2024-10-11 19:17:07 +0530

    to do card structure

--- src/components.js/ToDoList.js
+++ src/components.js/ToDoList.js
@@ -4,11 +4,13 @@ import ToDoCard from './ToDoCard'
 function ToDoList() {
   return (
     <>
+    <div className="m-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
     <ToDoCard></ToDoCard>
     <ToDoCard></ToDoCard>
     <ToDoCard></ToDoCard>
     <ToDoCard></ToDoCard>
     <ToDoCard></ToDoCard>
+    </div>
     </>
   )
 }
